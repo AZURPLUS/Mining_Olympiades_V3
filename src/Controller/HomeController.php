4d170118616694b5d52e5fd6de804c3fa7c3bf40
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-09-25 04:43:49 +0000

    Frontend WebTV

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,18 +16,18 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/{slug}', name:'app_frontend_page', methods: ['GET'])]
-    public function page($slug)
-    {
-        return match ($slug){
-            'presentation' => $this->render('frontend/presentation.html.twig'),
-            'programme' => $this->render('frontend/programme.html.twig'),
-            'photos' => $this->render('maquette/photo.html.twig'),
-            'contact' => $this->render('maquette/contact.html.twig'),
-            'webtv' => $this->render('maquette/webtv.html.twig'),
-            'participation' => $this->render('maquette/participation.html.twig'),
-            'participant' => $this->render('maquette/participant.html.twig'),
-            default => $this->redirectToRoute('app_maquette_accueil')
-        };
-    }
+//    #[Route('/{slug}', name:'app_frontend_page', methods: ['GET'])]
+//    public function page($slug)
+//    {
+//        return match ($slug){
+//            'presentation' => $this->render('frontend/presentation.html.twig'),
+//            'programme' => $this->render('frontend/programme.html.twig',['slug'=>null]),
+//            'photos' => $this->render('maquette/photo.html.twig'),
+//            'contact' => $this->render('maquette/contact.html.twig'),
+//            'webtv' => $this->render('frontend/webtv.html.twig'),
+//            'participation' => $this->render('maquette/participation.html.twig'),
+//            'participant' => $this->render('maquette/participant.html.twig'),
+//            default => $this->redirectToRoute('app_maquette_accueil')
+//        };
+//    }
 }
