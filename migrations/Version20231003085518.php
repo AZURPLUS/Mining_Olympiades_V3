db4c7f6c8d2db8b8af0c1dc8cac84e1933bd32f3
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-10-03 09:44:50 +0000

    Update all

---
+++ migrations/Version20231003085518.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231003085518 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE etudiant (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nom VARCHAR(255) DEFAULT NULL, prenoms VARCHAR(255) DEFAULT NULL, contact VARCHAR(255) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, filiere VARCHAR(255) DEFAULT NULL, niveau VARCHAR(255) DEFAULT NULL, reference VARCHAR(255) DEFAULT NULL, slug VARCHAR(255) DEFAULT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE etudiant');
+    }
+}
